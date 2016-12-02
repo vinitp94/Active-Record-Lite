221daf83bc963b0c6fc0504ddca9d6d648f52201
vinit_parikh <vinitp94@gmail.com>
2016-12-02 10:50:22 -0800

    Complete 00_attr_accessor_object.rb

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method "#{name}" do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method "#{name}=" do |n|
+        instance_variable_set("@#{name}", n)
+      end
+    end
   end
 end
