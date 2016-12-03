1d1ce1563fe60d3d2d2c1b6ffda878e309dcf6af
vinit_parikh <vinitp94@gmail.com>
2016-12-02 19:13:00 -0800

    Complete 04_associatable2.rb

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -46,23 +46,28 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name.to_s, options)
+    self.assoc_options[name] = BelongsToOptions.new(name.to_s, options)
+
     define_method "#{name}" do
+      options = self.class.assoc_options[name]
       foreign_key_value = self.send(options.foreign_key)
       options.model_class.where(id: foreign_key_value).first
     end
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name.to_s, self.to_s, options)
+    self.assoc_options[name] = HasManyOptions.new(name.to_s, self.name, options)
+
     define_method "#{name}" do
+      options = self.class.assoc_options[name]
       foreign_key_value = self.send(options.primary_key)
       options.model_class.where(options.foreign_key => foreign_key_value)
     end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
