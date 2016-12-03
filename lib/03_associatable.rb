b92791e6607469dede704e0c54f37f7851540338
vinit_parikh <vinitp94@gmail.com>
2016-12-02 16:58:24 -0800

    Complete 03_associatable.rb

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,55 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    # (@class_name.split /(?=[A-Z])/).map(&:downcase).join('_') + 's'
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = (name + '_id').to_sym
+    @class_name = name.capitalize
+    @primary_key = :id
+
+    @foreign_key = options[:foreign_key] if options.include?(:foreign_key)
+    @class_name = options[:class_name] if options.include?(:class_name)
+    @primary_key = options[:primary_key] if options.include?(:primary_key)
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = (self_class_name.downcase + '_id').to_sym
+    @class_name = name.singularize.camelcase
+    @primary_key = :id
+
+    @foreign_key = options[:foreign_key] if options.include?(:foreign_key)
+    @class_name = options[:class_name] if options.include?(:class_name)
+    @primary_key = options[:primary_key] if options.include?(:primary_key)
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name.to_s, options)
+    define_method "#{name}" do
+      foreign_key_value = self.send(options.foreign_key)
+      options.model_class.where(id: foreign_key_value).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name.to_s, self.to_s, options)
+    define_method "#{name}" do
+      foreign_key_value = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => foreign_key_value)
+    end
   end
 
   def assoc_options
@@ -46,5 +67,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
